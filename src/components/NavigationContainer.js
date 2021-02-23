c9e14101c8bf4e19212b4351366c3ba110f7c709
Carl Bergenhem <cbergenhem@gmail.com>
2021-02-23 14:50:30 -0500

    Add BottomNavigation
    Added navigation through routes as well

---
+++ src/components/NavigationContainer.js
@@ -0,0 +1,46 @@
+import './NavigationContainer.css'
+import { useRef, useState, useEffect, useCallback } from 'react';
+import { BottomNavigation } from '@progress/kendo-react-layout';
+import { useHistory } from 'react-router';
+import { withRouter } from 'react-router-dom';
+
+function NavigationContainer(props) {
+  const history = useHistory();
+
+  const items = [
+    { text: "HTML", id: 1, icon: "home", route: "/", selected: true },
+    { text: "Layout", id: 2, icon: "file-txt", route: "/LayoutSample" },
+    { text: "KendoReact Grid", id: 3, icon: "gear", route: "/GridExport" },
+    { text: "KendoReact TreeList", id: 4, icon: "dollar", route: "/TreeListExport" }
+  ];
+
+  let currentRoute = 0;
+  items.forEach((item, index) => {
+    if(item.route === history.location.pathname) {
+      currentRoute = index;
+    }
+  });
+  const [selectedId, setSelectedId] = useState(currentRoute);
+
+  const onSelect = (event) => {
+    setSelectedId(event.itemIndex);
+    history.push(event.itemTarget.route);
+  };
+
+  return (
+    <>
+      <div className="content">
+        {props.children}
+      </div>
+      <BottomNavigation
+        positionMode={'fixed'}
+        items={items.map(
+          (item) => ({ ...item, selected: items[selectedId].text === item.text })
+        )}
+        onSelect={onSelect}
+      />
+    </>
+  )
+}
+
+export default withRouter(NavigationContainer);
